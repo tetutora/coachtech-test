e5946117437cc5add9fcabeb4ba7c4ea65639ec9
kitano <beiyelinai01@gmail.com>
2024-12-19 10:53:39 +0900

    更新

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', [AuthController::class, 'index']);
+Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
+
+Route::middleware('auth')->group(function ()
+{
+    Route::get('/', [AuthController::class, 'index']);
 });
